ed0a8b5949b8eec1a840fb3f0d274379997af7e9
Ilya Chehov <ichehoc96@gmail.com>
2025-05-24 00:22:39 +0400

    add route contacts

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,8 +6,11 @@ class HomeController
 {
     public function index(): ?string
     {
-        return view()->render('index', ['pageTitle' => 'WORK!!!!', 'text' => 'QWERTY']);
-//        return view()->render('index', ['pageTitle' => 'WORK!!!!', 'text' => 'QWERTY'], 'test');
+        return view()->render('index', ['pageTitle' => 'Main | Test', 'text' => 'main']);
+    }
 
+    public function contacts(): ?string
+    {
+        return view()->render('contacts', ['pageTitle' => 'Contacts | Test', 'text' => 'contacts'], 'test');
     }
 }
--- config/routes.php
+++ config/routes.php
@@ -4,4 +4,6 @@ use My\Framework\Core\Router;
 
 return function (Router $router) {
     $router->get('/', [\My\Framework\App\Controllers\HomeController::class, 'index']);
+    $router->get('contacts', [\My\Framework\App\Controllers\HomeController::class, 'contacts']);
+
 };
